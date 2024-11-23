2b586430f545d2de3297d002210117dcc530f5ad
TieTie <nitailevi@gmail.com>
2024-11-23 13:44:14 +0000

    hey

--- src/Facility.cpp
+++ src/Facility.cpp
@@ -1,1 +1,2 @@
 #include "Facility.h"
+//FacilityType:: FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score): 
